b1cdc913cde318aeafd7b69acf5e4d7dfe053075
Gwon Seonggwang <g.passcode@gmail.com>
2018-12-11 05:17:30 +0900

    Grants permission for using camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import { View } from 'react-native';
+import { Permissions } from "expo";
+import * as React from "react";
+import { SafeAreaView, Text, View } from "react-native";
 
-export default class App extends React.Component {
-    public render() {
+type State = {
+  hasCameraPermission?: boolean;
+};
+
+export default class App extends React.Component<{}, State> {
+  public state: State = {};
+
+  public async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === "granted" });
+  }
+
+  public render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
       return <View />;
     }
+
+    return (
+      <SafeAreaView>
+        <Text>{hasCameraPermission ? "Grants permission to access camera" : "No access to camera"}</Text>
+      </SafeAreaView>
+    );
+  }
 }
